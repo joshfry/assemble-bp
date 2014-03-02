de4c9c910bc2a39fa87806a12c50e0cd0a1be6b6
Josh Fry <josh@bayphoto.com>
2014-03-01 18:15:48 -0800

    refactored and updated sass styles

--- src/js/init.js
+++ src/js/init.js
@@ -4,5 +4,11 @@
 // This is the last loading script. Initialize jQuery plugins, etc.
 // -------------------------------------------------------------------
 
+// fastclick.js
+printTitle('fastclick.js');
+window.addEventListener('load', function() {
+  FastClick.attach(document.body);
+}, false);
+
 // Init mobileMenu + options
 $('.mobile-menu').mobileMenu();
